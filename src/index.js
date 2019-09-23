01a03fc7b47d5869f2eb00a0ed30a253c1078955
github username <github email address>
2019-09-23 12:43:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener( "DOMContentLoaded", function () {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then(resp => resp.json())
+      .then(json => postImage(json) )
+  
+  function postImage(json) {
+    json.message.forEach(function(imageUrl) {
+      let image = document.createElement('img')
+      image.src = imageUrl
+      document.querySelector('#dog-image-container').appendChild(image)
+    });
+   };
+  })
+  
+  document.addEventListener( "DOMContentLoaded", function () {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(resp => resp.json())
+      .then(json => postBreed(json))
+  
+  
+    function postBreed(json) {
+      for (breed in json.message) {
+        let breedElement = document.createElement('li')
+        breedElement.textContent = breed
+        document.querySelector('#dog-breeds').appendChild(breedElement)
+  
+        breedElement.addEventListener('click', changeColor)
+      }
+    }
+   })
+  
+   function changeColor(event) {
+     event.target.style.color = 'blue';
+   }
